5f5d0760367f10cbfa2e93f6ef2725e3a8a36cea
Vighnesh Kadam <vighnesh.kadam.se@gmail.com>
2026-01-25 18:41:25 +0000

    first commit

--- src/main/java/com/example/AgentObservation.java
+++ src/main/java/com/example/AgentObservation.java
@@ -1,7 +1,21 @@
 package com.example;
 
+
 public class AgentObservation {
-    public String instanceId;
-    public double cpuUtilization;
-    public int highCpuDurationMinutes;
+
+    private final String resourceId;
+    private final double cpu;
+
+    public AgentObservation(String resourceId, double cpu) {
+        this.resourceId = resourceId;
+        this.cpu = cpu;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public double getCpu() {
+        return cpu;
+    }
 }
