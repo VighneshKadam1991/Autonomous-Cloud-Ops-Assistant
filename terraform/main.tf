92d69b0992559478fa5241560feaf7cbe88680f0
Vighnesh Kadam <vighnesh.kadam.se@gmail.com>
2026-01-25 18:06:53 +0000

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,93 +18,93 @@ resource "aws_dynamodb_table" "agent_memory" {
   }
 }
 
-# IAM Role for Lambda
-#resource "aws_iam_role" "lambda_role" {
-#  name = "${var.project_name}-lambda-role"
+ IAM Role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-lambda-role"
 
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17",
-#    Statement = [{
-#      Action    = "sts:AssumeRole"
-#      Effect    = "Allow"
-#      Principal = {
-#        Service = "lambda.amazonaws.com"
-#      }
-#    }]
-#  })
-#}
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
 
-# -----------------------------
-# IAM Policy (Agent Permissions)
-# -----------------------------
-#resource "aws_iam_role_policy" "agent_policy" {
-#  role = aws_iam_role.lambda_role.id
+ -----------------------------
+ IAM Policy (Agent Permissions)
+ -----------------------------
+resource "aws_iam_role_policy" "agent_policy" {
+  role = aws_iam_role.lambda_role.id
 
-#  policy = jsonencode({
-#    Version = "2012-10-17",
-#    Statement = [
-#      {
-#        Effect = "Allow",
-#        Action = [
-#          "cloudwatch:GetMetricData",
-#          "ec2:DescribeInstances",
-#          "ec2:RebootInstances",
-#          "dynamodb:GetItem",
-#          "dynamodb:PutItem"
-#        ],
-#        Resource = "*"
-#      }
-#    ]
-#  })
-#}
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "cloudwatch:GetMetricData",
+          "ec2:DescribeInstances",
+          "ec2:RebootInstances",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
 
-#resource "aws_iam_role_policy_attachment" "lambda_logs" {
-#  role       = aws_iam_role.lambda_role.name
-#  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-#}
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-# Upload Lambda JAR to S3
-#resource "aws_s3_object" "lambda_jar" {
-#  bucket = aws_s3_bucket.lambda_bucket.id
-#  key    = "lambda/lambda.jar"
-#  source = "../target/lambda.jar"
-#  etag   = filemd5("../target/lambda.jar")
-#}
+ Upload Lambda JAR to S3
+resource "aws_s3_object" "lambda_jar" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  key    = "lambda/lambda.jar"
+  source = "../target/lambda.jar"
+  etag   = filemd5("../target/lambda.jar")
+}
 
-# Lambda Function
-#resource "aws_lambda_function" "agent" {
-#  function_name = "${var.project_name}-agent"
-#  role          = aws_iam_role.lambda_role.arn
-#  runtime       = "java17"
-#  handler       = "com.example.TestAgentLambda::handleRequest"
-#  timeout       = 30
-#  memory_size  = 512
-#  s3_bucket = aws_s3_bucket.lambda_bucket.id
-#  s3_key    = aws_s3_object.lambda_jar.key
-#  environment {
-#      variables = {
-#        MEMORY_TABLE = aws_dynamodb_table.agent_memory.name
-#      }
-#    }
-#}
+ Lambda Function
+resource "aws_lambda_function" "agent" {
+  function_name = "${var.project_name}-agent"
+  role          = aws_iam_role.lambda_role.arn
+  runtime       = "java17"
+  handler       = "com.example.TestAgentLambda::handleRequest"
+  timeout       = 30
+  memory_size  = 512
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_jar.key
+  environment {
+      variables = {
+        MEMORY_TABLE = aws_dynamodb_table.agent_memory.name
+      }
+   }
+}
 
-#resource "aws_cloudwatch_event_rule" "agent_schedule" {
- # name                = "${var.project_name}-schedule"
- # schedule_expression = "rate(5 minutes)"
-#}
+resource "aws_cloudwatch_event_rule" "agent_schedule" {
+  name                = "${var.project_name}-schedule"
+  schedule_expression = "rate(5 minutes)"
+}
 
-#resource "aws_cloudwatch_event_target" "agent_target" {
- # rule = aws_cloudwatch_event_rule.agent_schedule.name
- # arn  = aws_lambda_function.lambda.arn
+resource "aws_cloudwatch_event_target" "agent_target" {
+  rule = aws_cloudwatch_event_rule.agent_schedule.name
+  arn  = aws_lambda_function.lambda.arn
 
-#}
+}
 
-#resource "aws_lambda_permission" "allow_eventbridge" {
-#  statement_id  = "AllowEventBridgeInvoke"
-#  action        = "lambda:InvokeFunction"
-#  function_name = aws_lambda_function.agent.function_name
-#  principal     = "events.amazonaws.com"
-#  source_arn    = aws_cloudwatch_event_rule.agent_schedule.arn
-#}
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowEventBridgeInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.agent.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.agent_schedule.arn
+}
 
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,35 @@
-#output "lambda_function_name" {
-#  value = aws_lambda_function.agent.function_name
-#}
-
-output "artifact_bucket" {
-  value = aws_s3_bucket.lambda_bucket.bucket
+# S3 Bucket Name
+output "lambda_bucket_name" {
+  description = "The name of the S3 bucket used to store the Lambda JAR"
+  value       = aws_s3_bucket.lambda_bucket.bucket
 }
 
-output "agent_memory_table" {
-  value = aws_dynamodb_table.agent_memory.name
+# Lambda JAR S3 Object Key
+output "lambda_jar_s3_key" {
+  description = "The S3 key for the uploaded Lambda JAR"
+  value       = aws_s3_object.lambda_jar.key
+}
+
+# DynamoDB Table Name
+output "dynamodb_table_name" {
+  description = "The name of the DynamoDB table for agent memory"
+  value       = aws_dynamodb_table.agent_memory.name
+}
+
+# IAM Role ARN
+output "lambda_role_arn" {
+  description = "The ARN of the Lambda execution role"
+  value       = aws_iam_role.lambda_role.arn
+}
+
+# Lambda Function ARN
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.agent.arn
+}
+
+# CloudWatch Event Rule ARN
+output "cloudwatch_event_rule_arn" {
+  description = "The ARN of the CloudWatch EventBridge rule that triggers the Lambda"
+  value       = aws_cloudwatch_event_rule.agent_schedule.arn
 }
